5a25c5bc0f11294d65f30f2bc2d439b130c24c3a
MaxBruno544 <bruno.lucian0544@gmail.com>
2018-09-29 12:37:05 -0300

    login3

--- cadastroUser.php
+++ cadastroUser.php
@@ -38,17 +38,38 @@
             </div>
     
             <div class="col-md-5">
-                <h3>Login</h3>
+                <h3>Cadasto de Usuário</h3>
                 <form action="autenticar.php" method="post">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="nome" aria-describedby="emailHelp" placeholder="Nome" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="sobrenome" aria-describedby="emailHelp" placeholder="Sobrenome" required>
+                    </div>
                     <div class="form-group">
                         <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" name="v_email" required>
                     </div>
                     <div class="form-group">
+                        <input type="text" class="form-control" id="nUsuario" aria-describedby="emailHelp" placeholder="Nome do Usuário" required>
+                    </div>
+                    <div class="form-group">
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" name="v_senha" required>
                     </div>
-                    <button type="submit" class="btn btn-info btn-lg btn-block">Entrar</button>
-                    <hr>
-                    <p class="text-center"><a href="cadastroUser.php">Criar conta</a> ou <a href="#">lembrar senha</a></p>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirmar senha" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="date" class="form-control" id="exampleInputPassword1" placeholder="Data de Nascimento" required>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline">
+                        <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                        <label class="custom-control-label" for="customRadioInline1">Masculino</label>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline">
+                        <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+                        <label class="custom-control-label" for="customRadioInline2">Feminino</label>
+                    </div><br><br>
+                    <button type="submit" class="btn btn-info btn-lg btn-block">Cadastrar</button>
                 </form>
             </div>
         </div>
