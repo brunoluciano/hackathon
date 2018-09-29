01cf06246bcfda0eede675f93347be33bfd7bbf1
MaxBruno544 <bruno.lucian0544@gmail.com>
2018-09-29 14:45:21 -0300

    login6

--- autenticar.php
+++ autenticar.php
@@ -18,7 +18,8 @@ else{
         <title>Erro Login</title>
     </head>
     <body>
-        <div class="modal" tabindex="-1" role="dialog">
+        <input type="hidden" data-toggle="modal" data-target="#myModal"> </button>
+        <div class="modal" id="myModal" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -30,6 +31,15 @@ else{
                 </div>
             </div>
         </div>
+        <script type="text/javascript">
+            $('#myModal').on('shown.bs.modal', function () {
+                $('#myInput').trigger('focus')
+                })
+        </script>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
     <?php
